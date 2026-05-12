3b17c371a8bc396949eac05d1206c76e629e51fe
kk5999214 <kk5999214@gmail.com>
2026-05-12 17:04:53 +0530

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -6,18 +6,33 @@ from pydantic import BaseModel
 class Settings(BaseModel):
     MAIN_KEY_B64: str = "WWcmdGMlREV1aDYlWmNeOA=="
     MAIN_IV_B64: str = "Nm95WkRyMjJFM3ljaGpNJQ=="
-    RELEASE_VERSION: str = "OB51"
+    RELEASE_VERSION: str = "OB53"
 
     USER_AGENT: str = "GarenaMSDK/4.0.19P10(I2404 ;Android 15;en;US;)" 
     
     OAUTH_URL: str = "https://ffmconnect.live.gop.garenanow.com/api/v2/oauth/guest/token:grant"
-    MAJOR_LOGIN_URL: str = "https://loginbp.ggwhitehawk.com/MajorLogin" 
     
     CLIENT_SECRET_PAYLOAD: str = (
         "2ee44819e9b4598845141067b281621874d0d5d7af9d8f7e00c1e54715b7d1e3&client_id=100067"
     )
     X_UNITY_VERSION: str = "2018.4.11f1"
-    TIMEOUT: float = 10.0
+    TIMEOUT: float = 15.0
+
+    # 11-Region Map for Login Server extraction
+    REGION_MAP: dict = {
+        "IND": "https://loginbp.ggpolarbear.com",
+        "BD":  "https://loginbp.ggpolarbear.com",
+        "ME":  "https://loginbp.common.ggbluefox.com",
+        "PK":  "https://loginbp.ggpolarbear.com",
+        "BR":  "https://loginbp.ggpolarbear.com",
+        "NA":  "https://loginbp.ggpolarbear.com",
+        "VN":  "https://loginbp.ggpolarbear.com",
+        "SG":  "https://loginbp.ggpolarbear.com",
+        "ID":  "https://loginbp.ggpolarbear.com",
+        "CIS": "https://loginbp.ggpolarbear.com",
+        "TH":  "https://loginbp.common.ggbluefox.com",
+        "GHOST": "https://loginbp.ggblueshark.com"
+    }
 
     @property
     def MAIN_KEY(self) -> bytes:
